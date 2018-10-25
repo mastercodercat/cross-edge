66d00686d906db1c60028ccc1537f63835cd8949
mastercodercat <mastercoderoy64@gmail.com>
2018-10-25 21:09:00 +0800

    [ASPP-75] Added env config

--- src/config/base.js
+++ src/config/base.js
@@ -4,6 +4,6 @@ export const AUTH_TOKEN_LOCAL_STORAGE_ITEM = `${ADEPT_EDGE_PREFIX}_auth_token`
 
 export const AUTH_EMAIL_LOCAL_STORAGE_ITEM = `${ADEPT_EDGE_PREFIX}_auth_email`
 
-export const API_BASE_URL = 'http://localhost:8000/api/v1'
+export const API_BASE_URL = `${process.env.REACT_APP_API_URL}/api/v1`
 
 export const DEFAULT_PAGE_SIZE = 10

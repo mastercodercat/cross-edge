7caa7633f8499bdd83050fb41ac2b4170111cf81
mastercodercat <mastercoderoy64@gmail.com>
2018-10-25 18:04:47 +0800

    [ASPP-75] Added tests for container components

---
+++ src/containers/ParentContainer/index.test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Immutable from 'immutable'
+
+import { ParentContainer } from './index'
+
+import {
+  REQUEST_INITIAL,
+  REQUEST_SUCCESS,
+} from 'constants.js'
+import { Site, SiteData } from 'store/modules/sites'
+import { sites } from 'test/fixtures/bpm'
+
+
+const ChildMock = (props) => <div>Child content</div>
+
+const props = {
+  type: 'site',
+  detail: SiteData(),
+  loadDetail: jest.fn(),
+  match: { params: { parentId: 10 } },
+  children: <ChildMock />
+}
+
+it('should show spinner when data not loaded', () => {
+  const wrapper = mount(<ParentContainer
+    {...props}
+  />)
+
+  expect(wrapper.find('.ant-spin')).not.toBeNull()
+  expect(props.loadDetail.mock.calls).toBeTruthy()
+})
+
+it('should show children when parent detail loaded', () => {
+  const localProps = Object.assign({}, props)
+  localProps.detail = SiteData({
+    data: Site(sites[0]),
+    state: REQUEST_SUCCESS,
+  })
+
+  const wrapper = mount(<ParentContainer
+    {...localProps}
+  />)
+
+  expect(wrapper.text()).toEqual(expect.stringContaining('Child content'))
+})
